d43df8ad5762e01ae10c6cd50e8d9a8d7b335914
faranak Sabet <faranaksabet@yahoo.com>
2020-12-01 19:52:16 -0800

    adding first version of the website

---
+++ data/info.js
@@ -0,0 +1,47 @@
+const info = {
+    navbarTitle: "Faranak Sabet",
+    name: "Faranak",
+    location: "Somewhere, TX",
+    zipCode: "00280",
+    qualities: "Student - Developer - Web Developer",
+    about: {
+        right: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        left: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    footerMessage: "Thanks for visiting my website",
+    resumeFile: "resume.pdf",
+    formId: "1FAIpQLSc7y10W2PkSsc0VHQVJKpiJQJJNO0UJzD-WJ4V_bWK1wt875g",
+    links: {
+        facebook: "https://www.facebook.com/faranak.ns/",
+        twitter: "https://twitter.com/FaranakSabet/",
+        linkedIn: "https://www.linkedin.com/in/faranak-sabet-b80961a2/",
+        Github: "https://github.com/FaranakSabet/",
+    },
+
+    projects: [
+        {
+            title: "Mern Stack",
+            img: "mern-stack.png",
+            description: "In this repository, you'll find a new React-based Google Books Search app. In this app, I created a React components, work with helper/util functions, and utilize React lifecycle methods to query and display books based on user searches. Here is also used Node, Express and MongoDB so that users can save books to review or purchase later.",
+            demo: "https://mern-stack-faranak.herokuapp.com/",
+            source: "https://github.com/FaranakSabet/mern-stack"
+        },
+        {
+            title: "Eat Da Burger",
+            img: "eat-da-burger.png",
+            description: "This application is a burger logger created with MySQL, Node, Express, Handlebars and a homemade ORM. It was designed the MVC pattern; Node and MySQL query and route data, and Handlebars generate the HTML. Eat-Da-Burger! is a restaurant app that lets users input the names of burgers they'd like to eat.",
+            demo: "https://eatburg3r.herokuapp.com/",
+            source: "https://github.com/FaranakSabet/Eat-Da-Burger-Node-Express-Handlebars"
+        },
+        {
+            title: "Weather App",
+            img: "weather.png",
+            description: "This web app fetches the weather of a searched city and saves your search history on localstorage",
+            demo: "https://github.com/FaranakSabet/Weather-dashboard",
+            source: "https://github.com/FaranakSabet/Weather-dashboard"
+        },
+    ]
+}
+
+
+module.exports = info
